56c4c3c47c16d1589afb38cc67394f5968244f12
Xaintailles <calixte.allier@gmail.com>
2023-02-16 08:23:35 +0100

    Function to parse SQL is working

--- main.py
+++ main.py
@@ -7,18 +7,36 @@ This is a temporary script file.
 
 import re
 
-to_search_for = ['FROM','LEFT JOIN']
+#TO DO: bug when handling LEFT JOIN, we get index of JOIN
+source_to_search_for = ['FROM','JOIN']
+destination_to_search_for = ['INSERT INTO']
 
-test_string = 'SELECT * FROM dwh.fact_order LEFT JOIN dwh.dim_order FROM'
+query = 'INSERT INTO dwh.fact_order (SELECT * FROM dwh.fact_order fo LEFT JOIN dwh.dim_order do on do.orderid = fo.orderid LEFT JOIN staging.all_orders so on so.source_order_id = do.sourceorderid)'
 
 def looking_for_table(searchable__string,semantic_elements):
-    all_indexes = []
+    start_index = []
+    
+    #here we grab every index for the first letter of the semantic elements
     for semantic_element in semantic_elements:
-        all_indexes.append([m.start() for m in re.finditer(semantic_element, searchable__string)])
+        start_index.append([m.end() + 1 for m in re.finditer(semantic_element, searchable__string)])
     
     #gives a list of lists, need to flatten
-    all_indexes = [item for sublist in all_indexes for item in sublist]
-    all_indexes.sort()
-    return all_indexes
+    start_index = [item  for sublist in start_index for item in sublist]
+    start_index.sort()
+    
+    end_index = []
+    
+    for index in start_index:
+        end_index.append(searchable__string.find(' ', index))
+        
+    all_tables = []
+    
+    for start, end in zip(start_index,end_index):
+        all_tables.append(searchable__string[start:end])
 
-print(looking_for_table(test_string,to_search_for))
+    return all_tables
+
+all_sources = looking_for_table(query,source_to_search_for)
+all_destinations = looking_for_table(query,destination_to_search_for)
+
+print(all_sources, all_destinations)
